d5a29cca718bb2ddb68577605cbaa0e8a480c11f
luckydamami <luckydamamii2410@gmail.com>
2025-06-27 14:45:21 +0530

    update server file

--- 02_Request_Response/app.js
+++ 02_Request_Response/app.js
@@ -1,33 +1,39 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.setHeader("Content-Type", "text/html");
-    res.write(`<h2>User Form</h2>
-    <form action="/submit">
+    res.write(`
+   <form action="/submit-details" method="post">
     <label for="username">Username:</label>
-    <input type="text" id="username" name="username" required>
+    <input type="text" name="username" required>
     <br><br>
 
     <label>Gender:</label><br>
-    <input type="radio" id="male" name="gender" value="male" required>
+    <input type="radio" name="gender" value="male" required>
     <label for="male">Male</label><br>
-    <input type="radio" id="female" name="gender" value="female">
+    <input type="radio" name="gender" value="female">
     <label for="female">Female</label><br>
-    <input type="radio" id="other" name="gender" value="other">
+    <input type="radio" name="gender" value="other">
     <label for="other">Other</label><br><br>
-
-    <input type="submit">
-  </form>
+    <input type="submit" value="Submit">
+  </form>   
   `);
     return res.end();
+  } else if (
+    req.url.toLowerCase() === "/submit-details" &&
+    req.method === "POST"
+  ) {
+    fs.writeFileSync("user-details.txt", "Lucky Damami");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
   }
   res.setHeader("Content-Type", "text/html");
-  res.write(`<h1>Something went wrong.....!</h1>`);
-  res.end();
+  res.write("<h1>Something went wrong!!!</h1>");
+  return res.end();
 });
 
-let PORT = 3000;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(
     `Your server start successfully on port http://localhost:${PORT}`
