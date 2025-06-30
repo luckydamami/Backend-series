41d8823ba0bcc8eb9907c9aea7c91230b6f89513
luckydamami <luckydamamii2410@gmail.com>
2025-06-30 18:03:17 +0530

    update file

--- 03_Parsing_Request/addition.js
+++
@@ -1,9 +0,0 @@
-const add = () => {
-  const body = [];
-  req.on("data", (chunk) => {
-    body.push(chunk);
-    console.log(body);
-  });
-};
-
-module.exports = add;
--- 03_Parsing_Request/server.js
+++ 03_Parsing_Request/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
-//const requestHandler = require("./user");
-const userRequestHandler = require("./test");
-const server = http.createServer(userRequestHandler);
+const requestHandler = require("./user");
+
+const server = http.createServer(requestHandler);
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(
