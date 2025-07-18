61f554e3601658da24b8d963a5ab7c9c2b21f242
luckydamami <luckydamamii2410@gmail.com>
2025-07-18 18:59:46 +0530

    Update some files

--- 10_MVC-Architecture/Controllers/Homes.js
+++ 10_MVC-Architecture/Controllers/Homes.js
@@ -1,15 +1,20 @@
+const Home = require("../Models/Home");
+
 exports.getAddHome = (req, res, next) => {
   console.log(req.body);
   res.render("addHome", { pageTitle: "Home Details" });
 };
 
 exports.postAddHome = (req, res, next) => {
-  registerdHomes.push(req.body);
-  console.log("Home Details : ", registerdHomes);
+  const { houseName, location, rooms, price, ratings, photoUrl } = req.body;
+  const home = new Home(houseName, location, rooms, price, ratings, photoUrl);
+  home.save();
+  console.log("Home Details : ", req.body);
   res.render("homeAdded", { pageTitle: "Home Added Successfully" });
 };
 
 exports.getHome = (req, res, next) => {
+  const registerdHomes = Home.fetchAll();
   res.render("home", {
     registerdHomes: registerdHomes,
     pageTitle: "Airbnb Home Page",
--- 10_MVC-Architecture/Models/Home.js
+++ 10_MVC-Architecture/Models/Home.js
@@ -1,15 +1,29 @@
+const rootDir = require("../utils/rootPath");
+const fs = require("fs");
+const path = require("path");
+
 const registerdHomes = [];
 
 module.exports = class Home {
-  constructor(houseName, price, rooms, location, ratings, photoUrl) {
+  constructor(houseName, location, rooms, price, ratings, photoUrl) {
     this.houseName = houseName;
-    this.price = price;
-    this.rooms = rooms;
     this.location = location;
+    this.rooms = rooms;
+    this.price = price;
     this.ratings = ratings;
     this.photoUrl = photoUrl;
   }
+
   save() {
     registerdHomes.push(this);
+    const dataPath = path.join(rootDir, "data", "homeData.json");
+
+    fs.writeFile(dataPath, JSON.stringify(registerdHomes), (error) => {
+      console.log("File write concluded!", error);
+    });
+  }
+
+  static fetchAll() {
+    return registerdHomes;
   }
 };
